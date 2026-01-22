6ec1d37ab8dcd96d80f733f95ee7a8a96525d901
Kenty <quentinserveaux.pro@gmail.com>
2026-01-22 22:12:34 +0400

    feat: Créer un profil

--- src/Form/ProfilType.php
+++ src/Form/ProfilType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ProfilType extends AbstractType
@@ -16,21 +17,21 @@ class ProfilType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pdp')
+            ->add('pdp', FileType::class, [
+                'label' => 'Photo de profil',
+                'mapped' => false,
+                'required' => false,
+            ])
             ->add('bio')
             ->add('utilisateur', EntityType::class, [
                 'class' => Utilisateur::class,
-                'choice_label' => 'id',
+                'choice_label' => 'nom',
             ])
             ->add('preferences', EntityType::class, [
                 'class' => Activite::class,
                 'choice_label' => 'nom',
                 'multiple' => true,
                 'expanded' => true,
-            ])
-            ->add('submit', SubmitType::class, [
-                'label' => 'Enregistrer',
-                'attr' => ['class' => 'btn btn-primary']
             ])
         ;
     }
